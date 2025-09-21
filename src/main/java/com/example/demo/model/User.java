f1685f0a64802f886845f211b7006e88f3751571
Aadi Dayal <dayalaadi123@gmail.com>
2025-09-21 15:24:02 +0530

    Added PassValidation

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,6 +1,7 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
@@ -16,16 +17,27 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty(message = "First name cannot be empty")
+    @Size(min = 2, max = 50, message = "First name must be 2-50 characters")
     @Column(nullable = false)
-
     private String firstName;
 
+    @NotEmpty(message = "Last name cannot be empty") 
+    @Size(min = 2, max = 50, message = "Last name must be 2-50 characters")
     @Column(nullable = false)
     private String lastName;
 
+    @Email(message = "Invalid email format")
+    @NotEmpty(message = "Email is mandatory")
     @Column(nullable = false, unique = true)
     private String email;
     
+    @NotEmpty(message = "Password is mandatory")
+    @Size(min = 8, message = "Password requires minimum 8 characters")
+    @Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+        message = "Password needs: uppercase, lowercase, digit, special character"
+    )
     @Column(nullable = false)
     private String password;
 }
